8666507f5cc9bf923e093ba195ed0205ed2f501f
gavin-gallant <gavin.gallant@mths.ca>
2024-11-05 11:26:12 -0500

    made code

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+*
+* Created by: Gavin Gallant
+* Created on: Oct 2024
+* This program controls two stepper motors and a sensor
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+while (true) {
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    // if distance <10 stop, back up and turn 
+    if (distanceToObject < 10) {
+        //stop
+        robotbit.StpCarMove(0, 48)
+        basic.showIcon(IconNames.No)
+        // back up
+        robotbit.StpCarMove(-10, 48)
+        robotbit.StpCarMove(0, 48)
+        //turn
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+    } else {
+        //forward 
+        robotbit.StpCarMove(10, 48)
+    }
+}
